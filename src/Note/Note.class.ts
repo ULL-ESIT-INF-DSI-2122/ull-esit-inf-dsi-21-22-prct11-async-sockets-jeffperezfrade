b630bd9d7abf04a4faa2944dff6cbf9f3d37aa8b
jeffperezfrade <alu0101038520@ull.edu.es>
2022-05-02 11:30:18 +0100

    adding Note class

--- src/Note/Note.class.ts
+++ src/Note/Note.class.ts
@@ -2,8 +2,36 @@
 export type Colors = 'yellow' | 'blue' | 'red' | 'blue';
 
 export class Note {
-  private userName: string;
-  private title: string;
-  private body: string;
-  private color: Colors;
+  private _user: string;
+  private _title: string;
+  private _body: string;
+  private _color: Colors;
+
+  constructor(
+      user: string,
+      title: string,
+      body: string,
+      color: Colors,
+  ) {
+    this._user = user;
+    this._title = title;
+    this._body = body;
+    this._color = color;
+  }
+
+  get user(): string {
+    return this._user;
+  }
+
+  get title(): string {
+    return this._title;
+  }
+
+  get body(): string {
+    return this._body;
+  }
+
+  get color(): Colors {
+    return this._color;
+  }
 }
