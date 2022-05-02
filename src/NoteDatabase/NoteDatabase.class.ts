0e30a638fb44f7eeba44137cd0e838760388e4d0
jeffperezfrade <alu0101038520@ull.edu.es>
2022-05-02 15:11:40 +0100

    modifyNote and deleteNote methods added

--- src/NoteDatabase/NoteDatabase.class.ts
+++ src/NoteDatabase/NoteDatabase.class.ts
@@ -22,5 +22,28 @@ export class NoteDatabase {
     console.log(chalk.green(`${note.title} note added to ${note.user} folder!`));
     return true;
   }
+
+  public modifyNote(note: Note): boolean {
+    if (!fs.existsSync(`database/${note.user}/${note.title}.json`)) {
+      console.log(chalk.red(`Error: ${note.title} does not exist!`));
+      return false;
+    } else {
+      const structureJSON: string = `{"title": "${note.title}", "body": "${note.body}", "color": "${note.color}"}`;
+      fs.writeFileSync(`database/${note.user}/${note.title}.json`, structureJSON);
+      console.log(chalk.green(`${note.title} modified!`));
+      return true;
+    }
+  }
+
+  public deleteNote(note: Note): boolean {
+    if (!fs.existsSync(`database/${note.user}/${note.title}.json`)) {
+      console.log(chalk.red(`Error: ${note.title} does not exist!`));
+      return false;
+    } else {
+      fs.rmSync(`database/${note.user}/${note.title}.json`);
+      console.log(chalk.green(`${note.title} note deleted!`));
+      return true;
+    }
+  }
 }
 
