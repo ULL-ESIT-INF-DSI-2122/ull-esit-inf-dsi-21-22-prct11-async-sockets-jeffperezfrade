ac50b2fce99b9b4faa63f03bb1d8407eba994a94
jeffperezfrade <alu0101038520@ull.edu.es>
2022-05-02 11:24:12 +0100

    adding Colors type

---
+++ src/Note/Note.class.ts
@@ -0,0 +1,9 @@
+
+export type Colors = 'yellow' | 'blue' | 'red' | 'blue';
+
+export class Note {
+  private userName: string;
+  private title: string;
+  private body: string;
+  private color: Colors;
+}
