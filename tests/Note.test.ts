ff722678e9bb066e19c0ac853727690e4f7713ba
jeffperezfrade <alu0101038520@ull.edu.es>
2022-05-04 16:21:20 +0100

    Note class tests

--- tests/Note.test.ts
+++ tests/Note.test.ts
@@ -3,25 +3,25 @@ import {expect} from 'chai';
 import {Note} from '../src/Note/Note.class';
 
 describe('Note class tests', () => {
-  const note = new Note('Jeff', 'Primera nota', 'Hola esta es mi primera nota', 'blue');
+  const nota = new Note('Jeff', 'Primera nota', 'Hola esta es mi primera nota', 'blue');
 
   it('The object is not null', () => {
-    expect(note).not.to.be.equal(null);
+    expect(nota).not.to.be.equal(null);
   });
 
-  it('note.user return Jeff', () => {
-    expect(note.user).to.be.equal('Jeff');
+  it('nota.user return Jeff', () => {
+    expect(nota.user).to.be.equal('Jeff');
   });
 
-  it('note.title should return Primera nota', () => {
-    expect(note.title).to.be.equal('Primera nota');
+  it('nota.title should return Primera nota', () => {
+    expect(nota.title).to.be.equal('Primera nota');
   });
 
-  it('note.body should return Hola esta es mi primera nota', () => {
-    expect(note.body).to.be.equal('Hola esta es mi primera nota');
+  it('nota.body should return Hola esta es mi primera nota', () => {
+    expect(nota.body).to.be.equal('Hola esta es mi primera nota');
   });
 
-  it('note.color should return blue', () => {
-    expect(note.color).to.be.equal('blue');
+  it('nota.color should return blue', () => {
+    expect(nota.color).to.be.equal('blue');
   });
 });
